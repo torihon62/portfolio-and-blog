04f8c7e3ae0fb1bded0bfdaa1fe87dbe8abec287
torimoto-shinya <torimoto-shinya@metawater.co.jp>
2021-06-06 15:15:28 +0900

    Layoutの修正漏れ

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -13,7 +13,7 @@ type Props = {
 const StaticPropsDetail = ({ item, errors }: Props) => {
   if (errors) {
     return (
-      <Layout title="Error | Next.js + TypeScript Example">
+      <Layout title="Error | Next.js + TypeScript Example" description={''}>
         <p>
           <span style={{ color: 'red' }}>Error:</span> {errors}
         </p>
@@ -22,7 +22,7 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
   }
 
   return (
-    <Layout title={`${item ? item.name : 'User Detail'} | Next.js + TypeScript Example`}>
+    <Layout title={`${item ? item.name : 'User Detail'} | Next.js + TypeScript Example`} description={''}>
       {item && <ListDetail item={item} />}
     </Layout>
   );
